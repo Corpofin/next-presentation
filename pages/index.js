9ce21dec38a20182608df6100fa8144ea8c1917d
Riccardo Canella <canella_riccardo@icloud.com>
2018-03-06 22:24:12 +0100

    animation with keyboards

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react'
 import styled from 'styled-components'
+import Router from 'next/router'
 
 import Slide from '../components/slide'
 import CustomLink from  '../components/link'
 
 
 export default class Index extends Component{
+    componentDidMount(){
+        window.prevSlide = ''
+        window.nextSlide = '/two'
+        setTimeout(()=>{
+            document.addEventListener("keydown", function (e) {
+                e.stopPropagation()
+                e.preventDefault()
+                if(e.keyCode === 37){
+                    Router.push(window.prevSlide, window.prevSlide, { shallow: true })
+                }
+                if(e.keyCode === 39){
+                    Router.push(window.nextSlide, window.nextSlide, { shallow: true })
+                }
+            });
+        },500)
+    }
     render(){
         return <Slide>
             <CustomLink type="next" href='/two'></CustomLink>
--- pages/two.js
+++ pages/two.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
 import styled from 'styled-components'
+import Router from 'next/router'
 
 import Slide from '../components/slide'
 import CustomLink from  '../components/link'
 
 export default class Index extends Component{
+    componentDidMount(){
+        window.prevSlide = '/two'
+        window.nextSlide = '/three'
+    }
     render(){
         return <Slide>
             <CustomLink type="next" href='/three'></CustomLink>
